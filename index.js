b772f5a5781b4c59eba63ad0f42f0a8d6813f353
Guillaume Migeon <guillaume.migeon.ext@boursorama.fr>
2016-12-23 17:15:42 +0100

    replace() isn't exported anymore

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 //Source: http://jsfiddle.net/unLSJ/
+function replacer(match, pIndent, pKey, pVal, pEnd) {
+    var key = '<span class=json-key>',
+        val = '<span class=json-value>',
+        str = '<span class=json-string>',
+        r   = pIndent || '';
+    if (pKey) r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
+    if (pVal) r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
+    return r + (pEnd || '');
+}
 module.exports = {
-    replacer: function (match, pIndent, pKey, pVal, pEnd) {
-        var key = '<span class=json-key>',
-            val = '<span class=json-value>',
-            str = '<span class=json-string>',
-            r   = pIndent || '';
-        if (pKey) r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
-        if (pVal) r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
-        return r + (pEnd || '');
-    },
     prettyPrint: function (obj) {
         var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg,
             self     = this;
         return '<pre class="json-pre"><code>' +
             JSON.stringify(obj, null, 3)
-            .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
-            .replace(/</g, '&lt;').replace(/>/g, '&gt;')
-            .replace(jsonLine, self.replacer) +
+                .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
+                .replace(/</g, '&lt;').replace(/>/g, '&gt;')
+                .replace(jsonLine, replacer) +
             '</code></pre>';
     }
 };
